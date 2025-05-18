1b945dc7cc3830b98dede8a81c0fa9236cabbb2e
Mirza Esaaf Shuja <mirzaesaaf@gmail.com>
2025-05-18 02:11:42 +0200

    feat: create module for toggling nvidia gpu

---
+++ modules/nixos/nvidia.nix
@@ -0,0 +1,32 @@
+{ lib, config, pkgs, ... }:
+let
+  inherit (lib) types mkOption mkIf;
+  cfg = config.hardware.nvidia;
+in
+{
+  options.hardware.nvidia = {
+    enable = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Enable the NVIDIA GPU";
+    };
+  };
+
+  config = mkIf (!cfg.enable) {
+    boot.extraModprobeConfig = ''
+        blacklist nouveau
+        options nouveau modeset=0
+      '';
+    services.udev.extraRules = ''
+      # Remove NVIDIA USB xHCI Host Controller devices, if present
+      ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
+      # Remove NVIDIA USB Type-C UCSI devices, if present
+      ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
+      # Remove NVIDIA Audio devices, if present
+      ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
+      # Remove NVIDIA VGA/3D controller devices
+      ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
+    '';
+    boot.blacklistedKernelModules = ["nouveau" "nvidia" "nvidia_drm" "nvidia_modeset"];
+  };
+}
