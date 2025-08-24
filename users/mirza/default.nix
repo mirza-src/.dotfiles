e1f5871fe7fab3694a1113c230f40e0f95a47dc9
Mirza Esaaf Shuja <mirzaesaaf@gmail.com>
2025-08-24 18:35:53 +0200

    chore: add obsidian

--- users/mirza/default.nix
+++ users/mirza/default.nix
@@ -42,6 +42,40 @@ in
 
   modules.hyprland.enable = true;
 
+  programs.obsidian = {
+    enable = true;
+    defaultSettings.corePlugins = [
+      "audio-recorder"
+      "backlink"
+      "bookmarks"
+      "canvas"
+      "command-palette"
+      "daily-notes"
+      "editor-status"
+      "file-explorer"
+      "file-recovery"
+      "global-search"
+      "graph"
+      "markdown-importer"
+      "note-composer"
+      "outgoing-link"
+      "outline"
+      "page-preview"
+      "properties"
+      # "publish"
+      "random-note"
+      "slash-command"
+      "slides"
+      "switcher"
+      # "sync"
+      "tag-pane"
+      "templates"
+      "word-count"
+      "workspaces"
+      "zk-prefixer"
+    ];
+  };
+
   programs.vscode = {
     enable = true;
     # mutableExtensionsDir = false;
@@ -61,36 +95,36 @@ in
       ++ (with pkgs.nix-vscode-extensions.vscode-marketplace; [
         ms-azuretools.vscode-containers
         ms-vscode.vscode-speech
-          ms-vscode-remote.remote-containers
-          jnoortheen.nix-ide
-          christian-kohler.path-intellisense
-          mkhl.direnv
-          github.copilot
-          github.vscode-github-actions
-          eamodio.gitlens
-          visualstudioexptteam.vscodeintellicode
-          gruntfuggly.todo-tree
-          usernamehw.errorlens
-          esbenp.prettier-vscode
-          redhat.vscode-yaml
-          redhat.vscode-xml
-          ms-python.python
-          golang.go
-          dbaeumer.vscode-eslint
-          bradlc.vscode-tailwindcss
-          ms-kubernetes-tools.vscode-kubernetes-tools
-          tamasfe.even-better-toml
-          redhat.java
-          vscjava.vscode-maven
-          vscjava.vscode-gradle
-          vscjava.vscode-java-test
-          vscjava.vscode-java-debug
-          vscjava.vscode-java-dependency
-          haskell.haskell
-          justusadam.language-haskell
-          tomoki1207.pdf
-          hashicorp.hcl
-          hashicorp.terraform
+        ms-vscode-remote.remote-containers
+        jnoortheen.nix-ide
+        christian-kohler.path-intellisense
+        mkhl.direnv
+        github.copilot
+        github.vscode-github-actions
+        eamodio.gitlens
+        visualstudioexptteam.vscodeintellicode
+        gruntfuggly.todo-tree
+        usernamehw.errorlens
+        esbenp.prettier-vscode
+        redhat.vscode-yaml
+        redhat.vscode-xml
+        ms-python.python
+        golang.go
+        dbaeumer.vscode-eslint
+        bradlc.vscode-tailwindcss
+        ms-kubernetes-tools.vscode-kubernetes-tools
+        tamasfe.even-better-toml
+        redhat.java
+        vscjava.vscode-maven
+        vscjava.vscode-gradle
+        vscjava.vscode-java-test
+        vscjava.vscode-java-debug
+        vscjava.vscode-java-dependency
+        haskell.haskell
+        justusadam.language-haskell
+        tomoki1207.pdf
+        hashicorp.hcl
+        hashicorp.terraform
         ms-vscode.wordcount
         theqtcompany.qt-core
         theqtcompany.qt-qml
