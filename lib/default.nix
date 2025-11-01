78eeafd68aa7f70b044e77b8e49eeab52fe20cff
Mirza Esaaf Shuja <mirzaesaaf@gmail.com>
2025-11-01 22:06:32 +0100

    refactor: improve entrypoint for modules

--- lib/default.nix
+++ lib/default.nix
@@ -1,20 +1,50 @@
 { lib, ... }:
 rec {
-  listDir = dir: lib.mapAttrsToList (name: _: name) (builtins.readDir dir);
+  capitalize =
+    str:
+    let
+      strList = lib.stringToCharacters str;
+    in
+    lib.toUpper (builtins.head strList)
+    + lib.concatStrings (lib.map lib.toLower (builtins.tail strList));
+
+  uncapitalize =
+    str:
+    let
+      strList = lib.stringToCharacters str;
+    in
+    lib.toLower (builtins.head strList)
+    + lib.concatStrings (lib.map lib.toLower (builtins.tail strList));
+
+  toCamelCase =
+    str:
+    let
+      strList = lib.splitString "-" str;
+    in
+    builtins.head strList + lib.concatStrings (lib.map capitalize (builtins.tail strList));
+
+  isNixFile = filename: lib.hasSuffix ".nix" filename;
 
   listNixModules =
     dir:
     lib.mapAttrsToList (name: _: lib.removeSuffix ".nix" name) (
-      lib.filterAttrs (name: value: value == "directory" || (builtins.match ".*\\.nix" name != null)) (
-        builtins.readDir dir
-      )
+      lib.filterAttrs (name: type: type == "directory" || isNixFile name) (builtins.readDir dir)
     );
 
-  listDirectories =
+  getModulesPerFile =
     dir:
-    lib.mapAttrsToList (name: _: name) (
-      lib.filterAttrs (_: value: value == "directory") (builtins.readDir dir)
-    );
+    lib.genAttrs (lib.remove "default" (listNixModules dir)) (module: import "${dir}/${module}.nix");
 
-  listDirectoryNames = dir: lib.forEach (listDirectories dir) (name: builtins.baseNameOf name);
+  createModuleEntryPoint =
+    dir:
+    let
+      modulesPerFile = getModulesPerFile dir;
+
+      default =
+        { ... }:
+        {
+          imports = builtins.attrValues modulesPerFile;
+        };
+    in
+    modulesPerFile // { inherit default; };
 }
--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,13 +1,1 @@
-{ self, lib, ... }:
-let
-  modulesPerFile = lib.genAttrs (lib.remove "default" (self.lib.listNixModules ./.)) (
-    module: import ./${module}.nix
-  );
-
-  default =
-    { ... }:
-    {
-      imports = builtins.attrValues modulesPerFile;
-    };
-in
-modulesPerFile // { inherit default; }
+{ self, ... }: self.lib.createModuleEntryPoint ./.
