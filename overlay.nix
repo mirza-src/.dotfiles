213fcc012471081b2915ed5938cd6c2edffcbe08
Mirza Esaaf Shuja <mirzaesaaf@gmail.com>
2026-04-12 13:23:28 +0200

    fix: update vscode insider version and fetch URLs

--- overlay.nix
+++ overlay.nix
@@ -9,13 +9,14 @@ let
   );
 in
 {
-  vscode = (prev.vscode.override { isInsiders = true; }).overrideAttrs (oldAttrs: {
+  vscode = (prev.vscode.override { isInsiders = true; }).overrideAttrs (oldAttrs: rec {
     src = prev.fetchurl {
-      name = "VSCode_Insiders_linux-x64.tar.gz";
-      url = "https://code.visualstudio.com/sha/download?build=insider&os=linux-x64";
-      sha256 = "sha256-yP9fYIKaJHI0wKvVzEN2NVEvd5zXcK235+cOMW96y48=";
+      # https://update.code.visualstudio.com/api/commits/{insider,stable}/{linux-x64,darwin} -- List commits for builds
+      # https://update.code.visualstudio.com/api/versions/commit:COMMIT_ID/{linux-x64,darwin}/{insider,stable} -- Get download URL for commit
+      url = "https://vscode.download.prss.microsoft.com/dbazure/download/insider/${version}/code-insider-x64-1771909975.tar.gz";
+      sha256 = "sha256-gseCpgZfEnk4XEjad2VylDfH72GVWdXXgc4d0RqMQgk=";
     };
-    version = "latest";
+    version = "88757eaab9989a8e9f1c6254b3fcdd9ce4d8bb98";
     buildInputs = oldAttrs.buildInputs ++ [
       prev.krb5
       prev.libsoup_3
