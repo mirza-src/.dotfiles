1f643a54153cfe0f58b57139d3db0911ed339648
Mirza Esaaf Shuja <mirzaesaaf@gmail.com>
2026-06-20 01:11:17 +0200

    feat: improve power management

--- modules/nixos/power.nix
+++ modules/nixos/power.nix
@@ -15,12 +15,42 @@ in
 
   config = mkIf cfg.enable {
     powerManagement.enable = true;
-    powerManagement.cpuFreqGovernor = "powersave";
     powerManagement.powertop.enable = true;
+    systemd.services.powertop.serviceConfig.ExecStart = "${pkgs.powertop}/bin/powertop --auto-tune";
 
-    services.thermald.enable = true;
-    services.power-profiles-daemon.enable = true;
     services.upower.enable = true;
+    services.thermald.enable = false;
+
+    services.tlp = {
+      enable = true;
+      pd.enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+        CPU_BOOST_ON_BAT = 0;
+        SCHED_POWERSAVE_ON_BAT = 1;
+
+        # Aggressive PCIe and SATA link power management
+        RUNTIME_PM_ON_BAT = "auto";
+        PCIE_ASPM_ON_BAT = "powersave";
+
+        # Disable wake-on-lan
+        WOL_DISABLE = "Y";
+      };
+    };
+
+    services.auto-cpufreq = {
+      enable = false;
+      settings = {
+        battery = {
+          governor = "powersave";
+          turbo = "never"; # Aggressively prevents CPU from boosting on battery
+        };
+        charger = {
+          governor = "performance";
+          turbo = "auto";
+        };
+      };
+    };
 
     environment.systemPackages = with pkgs; [
       powertop
